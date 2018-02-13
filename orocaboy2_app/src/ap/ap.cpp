b11c84e2a61c099a9cc49070add81ea117648fb4
chcbaram <chcbaram@paran.com>
2018-02-13 23:07:57 +0900

    LCD 로고 표시 추가

--- orocaboy2_app/src/ap/ap.cpp
+++ orocaboy2_app/src/ap/ap.cpp
@@ -9,6 +9,7 @@
 #include "hw.h"
 #include "ap_def.h"
 #include "rtos.h"
+#include "image/oroca_logo.h"
 
 const volatile __attribute__((section(".version_str"))) uint8_t fw_version_str[256] = _DEF_APP_VER_STR;
 const volatile __attribute__((section(".version_num"))) uint8_t fw_version_num[256] = _DEF_APP_VER_NUM;
@@ -19,6 +20,7 @@ const volatile __attribute__((section(".version_num"))) uint8_t fw_version_num[2
 //
 
 //-- Internal Functions
+void drawLogo(void);
 
 
 //-- External Functions
@@ -30,6 +32,8 @@ void apInit(void)
   timerSetPeriod(_DEF_TIMER2, 1000);
   timerAttachInterrupt(_DEF_TIMER2, swtimerISR);
   timerStart(_DEF_TIMER2);
+
+  drawLogo();
 }
 
 void apMain(void)
@@ -51,6 +55,32 @@ void apMain(void)
   }
 }
 
+void drawLogo(void)
+{
+  uint32_t x_offset;
+  uint32_t y_offset;
+  uint32_t step;
 
+
+  lcdSelectLayer(_DEF_LCD_LAYER2);
+
+  x_offset = (lcdGetXSize() - LOGO_WIDTH ) / 2;
+  y_offset = (lcdGetYSize() - LOGO_HEIGHT) / 2;
+
+
+  for (step = 0; step <= 200; step += 8)
+  {
+    lcdClear(0x0000);
+    for(uint16_t x = 0; x < LOGO_WIDTH; x++)
+    {
+      for(uint16_t y = 0; y < LOGO_HEIGHT; y++)
+      {
+        lcdDrawPixel(x + x_offset, y + y_offset + 199-step, (uint32_t)oroca_img[y*LOGO_WIDTH + x]);
+      }
+    }
+    lcdCopyLayer(_DEF_LCD_LAYER2, _DEF_LCD_LAYER1);
+  }
+
+}
 
 
